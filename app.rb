f5d9e7f1f86b2e1a60d77b165aad4b5c3d6786cb
Lulu Davies <ldavies3004@gmail.com>
2013-07-30 18:20:59 +0100

    a lot

--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
 require 'sinatra'
 
 get '/' do
-    erb :index
+"Hello There!"
 end
+
+get '/:to' do
+    @to = params[:to]
+    erb :index1
+end
+
+get '/:to/:age' do 
+    @to = params[:to]
+    @age = params[:age]
+    erb :index1
+end
+
+get '/:to/:age/:from' do 
+    @to = params[:to]
+    @age = params[:age]
+    @from = params[:from]
+    erb :index1
+end
+
+
+
