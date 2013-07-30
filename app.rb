d479953df6301d5c851854583a6a671a6052352f
Lulu Davies <ldavies3004@gmail.com>
2013-07-30 18:27:20 +0100

    changes name of index

--- app.rb
+++ app.rb
@@ -6,20 +6,20 @@ end
 
 get '/:to' do
     @to = params[:to]
-    erb :index1
+    erb :index
 end
 
 get '/:to/:age' do 
     @to = params[:to]
     @age = params[:age]
-    erb :index1
+    erb :index
 end
 
 get '/:to/:age/:from' do 
     @to = params[:to]
     @age = params[:age]
     @from = params[:from]
-    erb :index1
+    erb :index
 end
 
 
